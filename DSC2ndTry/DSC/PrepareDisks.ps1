bdf0f0df03983eb5562cc1ff8457d3d36f2c5c67
ascdev2016 <andre.schmidt@direkt-gruppe.de>
2016-08-12 10:39:38 +0200

    PrepareDisk SQLAdded

---
+++ DSC2ndTry/DSC/PrepareDisks.ps1
@@ -0,0 +1,19 @@
+Configuration DataDisk
+    {
+    Import-DSCResource -ModuleName xStorage
+    Node localhost
+    {
+        xWaitforDisk Disk2
+            {
+            DiskNumber = 2
+            RetryIntervalSec = 60
+            RetryCount = 60
+            }
+        xDisk FVolume
+            {
+            DiskNumber = 2
+            DriveLetter = ‘F’
+            FSLabel = ‘Data’
+            }
+    }
+}
